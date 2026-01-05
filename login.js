c1b5d10c97ae1f1f4384a51dc8e69a597fae5f6f
Aditya Ranjan Sinha <aditya.ranjan.cs@gmail.com>
2026-01-05 11:02:36 +0530

    My First Project

---
+++ login.js
@@ -0,0 +1,16 @@
+// const id=1234  // const command gives a constant value and you can't change it later
+// let password="09" // let is used to declare a variable
+// console.table([password, id])
+
+// let password=69
+// console.log(typeof password);
+// password=String(password)
+// console.log(typeof password);
+// console.log(password);
+
+// let password = null
+// console.log(password);
+
+// let password = undefined
+// console.log(password);
+
